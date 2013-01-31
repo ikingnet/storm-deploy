9373444c89b84247eb4dcc673b6e15a9a4f021c5
ikingnet <zhangzy@xy.com>
2013-01-31 11:44:04 +0800

    add drpc and scheduler meta config

--- setup.sh
+++ setup.sh
@@ -77,6 +77,10 @@ done < $DEPLOY_HOME/zkserver.list
 echo "storm.zookeeper.port: 2181" >> storm-0.8.1/conf/storm.yaml
 echo 'storm.local.dir: '${PWD}'/stormdata' >> storm-0.8.1/conf/storm.yaml
 echo 'nimbus.host: "'`head -n1 $DEPLOY_HOME/nimbus.host`'"' >> storm-0.8.1/conf/storm.yaml
+echo 'drpc.servers: ' >> storm-0.8.1/conf/storm.yaml
+echo ' - "'`head -n1 $DEPLOY_HOME/nimbus.host`'"' >> storm-0.8.1/conf/storm.yaml
+echo 'supervisor.scheduler.meta:' >> storm-0.8.1/conf/storm.yaml
+echo ' name: "'${HOSTNAME}'"' >> storm-0.8.1/conf/storm.yaml
 
 sed -i "/export STORM_HOME=/d" /etc/profile
 sed -i "/export PATH=\$STORM_HOME\/bin:\$PATH/d" /etc/profile
